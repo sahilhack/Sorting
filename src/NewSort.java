ca67135faa081f0e3a778296a51429e2365f1ccb
student <student@10.14.0.226>
2017-08-24 10:10:23 +0530

    newsort

--- src/NewSort.java
+++ src/NewSort.java
@@ -1,9 +1,44 @@
 
 public class NewSort {
-
-	public static void main(String[] args) {
+	static void bubbleSort(int[] arr)  {
 		// TODO Auto-generated method stub
-
+       
+		int n=arr.length;
+       
+       int temp=0;
+       for(int i=0;i<n;i++)
+       {
+    	   for(int j=0;j<n;j++)
+    	   {
+    		   if(arr[i]<arr[j]){
+    			   temp=arr[i];
+    				arr[i]=arr[j];
+    				arr[j]=temp;
+    		   }
+    	   }
+       }
 	}
+	
+
+
+public static void main(String[] args){
+	int arr[]={32,7,4,14,4,30,16};
+	System.out.println("Before Sorting:");
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println(arr[i]+"");
+	}
+	System.out.println();
+	
+BubbleSort sahil = new BubbleSort();
+sahil.bubbleSort(arr);
+
+System.out.println("After Sorting:");
+for(int i=0;i<arr.length;i++){
+	System.out.println(arr[i]+"");
+}
+}
+
+	
 
 }
